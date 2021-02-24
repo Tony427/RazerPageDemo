26ace6dbde811917d4ca4fb0a9fc85bbde784d2c
TonyTsai <tony@unifiedpractice.com>
2021-02-24 14:57:07 +0800

    add form test example from darkthread

---
+++ RazorPagesMovie/Pages/FormTest.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages
+{
+    public class FormTestModel : PageModel
+    {
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string Now { get; set; }
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Now = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        public void OnPostEngGreet()
+        {
+            Message = $"Hello, {Name}";
+        }
+
+        public void OnPostChtGreet()
+        {
+            Message = $"{Name}, 您好";
+        }
+    }
+}
