0ecf4c90e22396c8840ced78ca34459b0c86269a
TonyTsai <tony@unifiedpractice.com>
2021-02-24 15:55:35 +0800

    add some description

--- RazorPagesMovie/Pages/FormTest.cshtml.cs
+++ RazorPagesMovie/Pages/FormTest.cshtml.cs
@@ -12,22 +12,28 @@ namespace RazorPagesMovie.Pages
         [BindProperty]
         public string Name { get; set; }
         [BindProperty]
-        public string Now { get; set; }
-        public string Message { get; set; }
+        public string Now => DateTime.Now.ToString("HH:mm:ss");
+        public string Message => $"{HelloPrefix}{Name}";
+        public string HelloPrefix { get; set; }
 
         public void OnGet()
         {
-            Now = DateTime.Now.ToString("HH:mm:ss");
         }
 
         public void OnPostEngGreet()
         {
-            Message = $"Hello, {Name}";
+            HelloPrefix = "Hello, ";
         }
+
+        //跟同步方法只能有一個
+        //public async Task OnPostEngGreetAsync()
+        //{
+        //    HelloPrefix = "HelloAsync, ";
+        //}
 
         public void OnPostChtGreet()
         {
-            Message = $"{Name}, 您好";
+            HelloPrefix = "您好, ";
         }
     }
 }
